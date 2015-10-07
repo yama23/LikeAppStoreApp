7c70a5221c774167db56aaf78f801a2e929164b3
yama23 <ym3215@mitsuhiko-no-macbook-pro.local>
2015-10-07 17:23:22 +0900

    Initial Commit

---
+++ LikeAppStoreApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LikeAppStoreApp
+//
+//  Created by Mitsuhiko Yamamoto on 2015/10/07.
+//  Copyright © 2015年 Fusic. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
